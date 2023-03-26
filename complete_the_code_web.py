baa386113efe4d1adcd79d26ee0f9c4bbf0f8225
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-03-26 11:33:23 +0530

    Update complete_the_code_web.py

--- complete_the_code_web.py
+++ complete_the_code_web.py
@@ -4,15 +4,15 @@ import random
 #-------------data--------------
 code_snippets = {
     "snippet1": {
-        "code": "#include <stdio.h>\n\nint main() {\n int num = 10;\n int [BLANK];\n ptr = #\n\n printf('The value of num is: %d', *ptr);\n\n return 0;\n}",
+        "code": "#include <stdio.h>\n\nint main() {\n int num = 10;\n int *ptr;\n ptr = #\n\n printf('The value of num is: %d', *ptr);\n\n return 0;\n}",
         "answer": "*ptr"
     },
     "snippet2": {
-        "code": "#include <stdio.h>\n\nint main() {\n int i = 0;\n [BLANK] (i < 5) {\n printf(''%d '', i);\n i++;\n }\n\n return 0;\n}",
+        "code": "#include <stdio.h>\n\nint main() {\n int i = 0;\n while (i < 5) {\n printf(''%d '', i);\n i++;\n }\n\n return 0;\n}",
         "answer": "while"
     },
     "snippet3": {
-        "code": "#include <stdio.h>\n\nint main() {\n int num = 10;\n int [BLANK] = #\n\n printf('The address of num is: %p', ptr);\n\n return 0;\n}",
+        "code": "#include <stdio.h>\n\nint main() {\n int num = 10;\n int *ptr = #\n\n printf('The address of num is: %p', ptr);\n\n return 0;\n}",
         "answer": "*ptr"
     },
     "snippet4": {
@@ -20,15 +20,15 @@ code_snippets = {
         "answer": "sum"
     },
     "snippet5": {
-        "code": "#include <stdio.h>\n\nint main() {\n int num1 = 5, num2 = 10;\n int [BLANK] = &num1;\n *ptr = 15;\n\n printf('The value of num1 is: %d', num1);\n\n return 0;\n}",
+        "code": "#include <stdio.h>\n\nint main() {\n int num1 = 5, num2 = 10;\n int *ptr = &num1;\n *ptr = 15;\n\n printf('The value of num1 is: %d', num1);\n\n return 0;\n}",
         "answer": "*ptr"
     },
     "snippet6": {
-        "code": "#include <stdio.h>\n\nint main() {\n int arr[] = {1, 2, 3, 4, 5};\n int n = sizeof(arr) / sizeof(arr[0]);\n int [BLANK];\n for (int i = 0; i < n; i++) {\n rev_arr[i] = arr[n - i - 1];\n }\n return 0;\n}",
+        "code": "#include <stdio.h>\n\nint main() {\n int arr[] = {1, 2, 3, 4, 5};\n int n = sizeof(arr) / sizeof(arr[0]);\n int rev_arr;\n for (int i = 0; i < n; i++) {\n rev_arr[i] = arr[n - i - 1];\n }\n return 0;\n}",
         "answer": "rev_arr"
     },
     "snippet7": {
-        "code": "#include <stdio.h>\n\nint main() {\n int num = 10;\n int *ptr = #\n printf('The value of num is: %d', [BLANK]);\n return 0;\n}",
+        "code": "#include <stdio.h>\n\nint main() {\n int num = 10;\n int *ptr = #\n printf('The value of num is: %d', *ptr);\n return 0;\n}",
         "answer": "*ptr"
     }
 }
