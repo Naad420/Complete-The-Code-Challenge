4dc24c2caa962f6c78cacd03677c4574cea1aa0e
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-03-26 12:00:01 +0530

    Update last.py

--- last.py
+++ last.py
@@ -2,9 +2,9 @@ import streamlit as st
 import random
 from code_snippets import Snippets
 
-
 data = Snippets()
 code, key = data.fetch_random_snippet()
+
 #-----------settings------------
 PAGE_TITLE = "</>Complete the Code</>"
 PAGE_ICON = ":fire:"
@@ -15,18 +15,20 @@ WRONG_EMOJI = ':joy:'
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON, layout=layout)
 st.title("MUJ IEEE CS: BATTLESHIP :collision:")
 st.title("</> Complete_the_code </>")
+
 #-------------------------------
-#-------------------------------
-code.replace(key, "____", 1) # only replacing first occurance
+code = code.replace(key, "____", 1) # only replacing first occurrence
+
 #-------------------------------
 col1 = st.code(code, language='c')
 user_text_input = st.text_input(label="Enter Code for Blank", key="#t1")
 submit_button = st.button("Submit")
+
 #-------------------------------
 if submit_button:
-    if user_text_input == user_text_input:
+    if user_text_input == key:
         st.write(f"Correct {CORRECT_EMOJI}: " + str(user_text_input))
     else:
         st.write(f"Wrong {WRONG_EMOJI}: " + str(user_text_input))
         st.write("Expected: ")
-        col2 = st.code(code.replace("____", key), language='c')
+        st.code(code.replace("____", key), language='c')
