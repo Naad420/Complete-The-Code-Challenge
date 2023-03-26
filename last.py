2df25a873c0406b58e16e6b205cbbc87f26a61b0
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-03-26 12:11:50 +0530

    Update last.py

--- last.py
+++ last.py
@@ -17,36 +17,27 @@ st.title("MUJ IEEE CS: BATTLESHIP :collision:")
 st.title("</> Complete_the_code </>")
 #-------------------------------
 #-------------------------------
-code = code.replace(key, "____", 1) # only replacing first occurrence
+code.replace(key, "____", 1) # only replacing first occurance
 #-------------------------------
 col1 = st.code(code, language='c')
-user_text_input = st.text_input(label="Enter Code for Blank", key="#t1")
 submit_button = st.button("Submit")
-show_restart_button = False
+user_text_input = st.text_input(label="Enter Code for Blank", key="#t1")
 #-------------------------------
 if submit_button:
     if user_text_input == key:
         st.write(f"Correct {CORRECT_EMOJI}: " + str(user_text_input))
-        show_restart_button = True
+        restart_button = st.button("Restart")
+        if restart_button:
+            code, key = data.fetch_random_snippet()
+            code.replace(key, "____", 1)
+            col1 = st.code(code, language='c')
     else:
         st.write(f"Wrong {WRONG_EMOJI}: " + str(user_text_input))
-        st.write("Expected: ")
+        st.write("Expected: " + key)
         col2 = st.code(code.replace("____", key), language='c')
 
-    # clear input
-    user_text_input = ""
+    user_text_input = st.text_input(label="Enter Code for Blank", key="#t1", value="")
 
-if show_restart_button:
-    restart_button = st.button("Restart")
-    if restart_button:
-        code, key = data.fetch_random_snippet()
-        code = code.replace(key, "____", 1)
-        show_restart_button = False
-        user_text_input = ""
-        col1 = st.code(code, language='c')
-
-#-------------------------------
-st.text_input('Input Text', value=user_text_input, key='#t1')
 
 
 # import streamlit as st
