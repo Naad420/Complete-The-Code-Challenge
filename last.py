384471236b464d66ab01398266fc1cbe34460b24
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-03-26 12:08:57 +0530

    Update last.py

--- last.py
+++ last.py
@@ -17,7 +17,7 @@ st.title("MUJ IEEE CS: BATTLESHIP :collision:")
 st.title("</> Complete_the_code </>")
 #-------------------------------
 #-------------------------------
-code.replace(key, "____", 1) # only replacing first occurance
+code = code.replace(key, "____", 1) # only replacing first occurance
 #-------------------------------
 col1 = st.code(code, language='c')
 user_text_input = st.text_input(label="Enter Code for Blank", key="#t1")
